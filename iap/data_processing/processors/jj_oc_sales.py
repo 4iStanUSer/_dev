772409d093814b8b62b883bb121df2fc9b4f163b
Voronov Andrey <andrey.voronov@4igrowth.com>
2016-07-29 15:38:27 +0300

    minor fixes

--- iap/data_processing/processors/jj_oc_sales.py
+++ iap/data_processing/processors/jj_oc_sales.py
@@ -1,6 +1,5 @@
 from iap.repository.warehouse import exceptions as ex
-from iap.data_processing.processors.common import get_last_col, \
-    get_cell_range
+from iap.data_processing.processors.common import get_last_col
 import collections
 
 
@@ -21,11 +20,7 @@ def jj_oc_sales(wb, info, meta_cols, name_col_num, dates_info, prop_info,
     for key, val in meta_cols.items():
         if val != '':
             header_row[key].value = val
-    # header_row = change_dates_names(header_row, start_dates_col,
-    #                                           end_dates_col,
-    #                                           dates_info['date_name_rows'], ws)
     dates_rows = get_dates_rows(dates_info['date_name_rows'], ws)
-    # dates = get_dates(start_dates_col, end_dates_col, header_row)
     for row_index in range(start_data_row, ws.nrows):
         this_row = ws.row(row_index)
         meta = get_meta(header_row, this_row, meta_cols)
@@ -38,13 +33,6 @@ def jj_oc_sales(wb, info, meta_cols, name_col_num, dates_info, prop_info,
         output.append(new_row)
         # desc_val = str(ws.cell(row_index, 0).value)
     return output
-
-
-def get_dates(start_col, end_col, header_row):
-    dates = []
-    for col in range(start_col, end_col):
-        dates.append(header_row[col].value)
-    return dates
 
 
 def get_values(dates_rows_list, dates_func, data_row, start_col, end_col):
@@ -63,23 +51,6 @@ def get_dates_rows(dates_rows_list, ws):
     for row_index in dates_rows_list:
         dates_rows.append(ws.row(row_index))
     return dates_rows
-
-
-def change_dates_names(header_row, start_col, end_col, dates_cols, ws):
-    start_row = min(dates_cols)
-    end_row = max(dates_cols)
-    dates_names_range = get_cell_range(0, start_row, end_col,
-                                       end_row + 1, ws)
-    for i in range(start_col, end_col):
-        new_name = ''
-        for idx, val in enumerate(dates_cols):
-            if idx == 0:
-                new_name = str(dates_names_range[idx][i].value)
-            else:
-                new_name = new_name + str(' ') + \
-                           str(dates_names_range[idx][i].value)
-        header_row[i].value = new_name
-    return header_row
 
 
 def get_meta(header_row, row, meta_info):
