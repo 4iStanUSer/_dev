da03c3d9c253e010980e5383143370c160ae7452
Vira Gorkovska <vira.gorkovska@4igrowth.com>
2016-08-16 13:03:27 +0300

    added possibility to get data from backup storage, create instance and add this instance for the user to the memory

--- iap/common/RunTimeCollection/__init__.py
+++ iap/common/RunTimeCollection/__init__.py
@@ -1,8 +1,12 @@
 from . import exceptions as ex
+from iap.repository.storage import *
 
 
 class RunTimeCollection:
     saved = {}
+
+    def __init__(self, tool_name):
+        self.tool_name = tool_name
 
     def add(self, user_id, instance):
         if user_id not in self.saved:
@@ -15,5 +19,33 @@ class RunTimeCollection:
             del self.saved[user_id]
 
     def get(self, user_id):
-        instance = self.saved[user_id]
+        if user_id in self.saved:
+            instance = self.saved[user_id]
+        else:
+            instance = self.__lookup_in_storage(user_id)
+            self.add(user_id, instance)
         return instance
+
+    def __lookup_in_storage(self, user_id):
+        s = Storage()
+        data = s.load_backup(user_id, self.tool_name, 'default')
+        if data:
+            instance = self.__create_instance(self.tool_name)
+            instance.load(data)
+            return instance
+        else:
+            raise ex.BackupNotFound(user_id, self.tool_name)
+
+    def __create_instance(self, tool_name):
+        if tool_name == 'forecasting':
+            instance = ForecastForDemoInstance()
+        else:
+            raise ex.InstanceCanNotBeCreated(tool_name)
+        return instance
+
+
+class ForecastForDemoInstance:
+    data = {}
+
+    def load(self, data):
+        self.data = data
--- iap/common/RunTimeCollection/exceptions.py
+++ iap/common/RunTimeCollection/exceptions.py
@@ -7,3 +7,27 @@ class InstanceAlreadyExistsError(Exception):
 
     def __init__(self, user_id):
         self.user_id = user_id
+
+
+class BackupNotFound(Exception):
+    '''Raised when backup for current user and specified tool not found in backups storage directory
+
+    Attributes:
+        user_id -- user ID
+        tool_name -- tool name
+    '''
+
+    def __init__(self, user_id, tool_name):
+        self.user_id = user_id
+        self.tool_name = tool_name
+
+
+class InstanceCanNotBeCreated(Exception):
+    '''Raised when tool name passed doesn't have corresponding class instance to be created
+
+    Attributes:
+        tool_name -- tool name
+    '''
+
+    def __init__(self, tool_name):
+        self.tool_name = tool_name
