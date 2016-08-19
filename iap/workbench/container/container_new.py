b2f03a227a1271b883f12e59afaaa640eefa51c7
Dmitry Radkevich <dradkevich@4iconsult.com>
2016-08-19 16:43:53 +0300

    temporary commit

--- iap/workbench/container/container_new.py
+++ iap/workbench/container/container_new.py
@@ -1,4 +1,4 @@
-import numpy as np
+from .timelines_new import TimeLineManager
 
 class Container:
 
@@ -6,6 +6,7 @@ class Container:
         self._root = CEntity()
         self._last_id = 0
         self._nodes_dict = {'0': self._root}
+        self.timeline = TimeLineManager()
 
     def add_entity(self, path):
         self._root.add_node_by_path(path, 0)
@@ -20,13 +21,28 @@ class Container:
         pass
 
 
+
+
+
+
+class DataStorage:
+    def __init__(self):
+        variables = {'name': name, 'time_series': {'ts_name': []}}
+
+    def add_variable(self, name):
+        pass
+
+
+
+
+
 class CEntity:
 
     def __init__(self, name):
         self._name = name
         self._parents = None
         self._children = None
-        self._variables = None
+        self._data_storage = DataStorage()
 
     @property
     def name(self):
@@ -100,161 +116,71 @@ class CEntity:
             return node
 
 
+
+
+
+
+
+
 class CVariable:
 
     def __init__(self, name, entity):
-        self._name = name
-        self._entity = entity
-        self._time_series = None
-        self._default_value = None
+
 
     @property
     def name(self):
-        return self._name
+        pass
 
     @name.setter
     def name(self, name):
-        if self._name == name:
-            return
-        if name in self._entity.get_variables_names():
-            raise Exception
-        self._name = name
+        pass
 
     @property
     def default_value(self):
-        return self._default_value
+        pass
 
     def get_time_series(self, ts_name):
-        for ts in self._time_series:
-            if ts.name == ts_name:
-                return ts
-        return None
+        pass
 
     def force_time_series(self, ts_name):
-        for ts in self._time_series:
-            if ts.name == ts_name:
-                return ts
-        #TODO add getter from timescale
-        time_scale = None
-        if time_scale is None:
-            raise Exception
-        time_series = CTimeSeries(ts_name, self)
-        time_series._time_scale = time_scale
-        self._time_series.append(time_series)
-        return time_series
+        pass
+
 
 
 class CTimeSeries:
 
-    def __init__(self, name, variable, length):
-        self._name = name
-        self._variable = variable
-        self._start = None
-        self._end = None
-        self._values = [self._variable.default_value] * length
+    def __init__(self, node_data, var_name, ts_name):
+        self._node_data = node_data
+        self._var_name = var_name
+        self._ts_name = ts_name
 
     @property
     def name(self):
-        return self._name
+
+        return self._ts_name
 
     @property
     def start_point(self):
-        return self._start
+        return self._node_data.get_start(self._var_name, self._ts_name)
 
     @property
     def end_point(self):
-        return self._end
+        return self._node_data.get_end(self._var_name, self._ts_name)
 
     def set_values(self, start_label, values):
-
-
-
-
-        # Get timestamps for new values.
-        new_stamps = self._time_scale.get_stamps_by_start_label(start_label,
-                                                                len(values))
-        new_start = new_stamps[0]
-        new_end = new_stamps[-1]
-        # Check and adjust current borders.
-        if self._start is None:
-            self._start = new_start
-            self._end = new_end
-        else:
-            # Check for gaps
-            gap_timestamps = []
-            # Gap to the right.
-            if self._end < new_start:
-                gap_timestamps = \
-                    self._time_scale.get_stamps_for_range(self._end,
-                                                          new_start)[1:-1]
-            # Gap to the left.
-            if self._start > new_end:
-                gap_timestamps = \
-                    self._time_scale.get_stamps_for_range(new_end,
-                                                          self._start)[1:-1]
-            # Shift borders according to latest data.
-            if new_start < self._start:
-                self._start = new_start
-            if new_end > self._end:
-                self._end = new_end
-            # Fill gaps with default values
-            for time_point in gap_timestamps:
-                point = Value(timestamp=time_point,
-                              data_type=self._variable.data_type)
-                point.set(self._variable.default_value)
-                self._values.append(point)
-
-        # Set new values.
-        ssn = object_session(self)
-        # Get existing points from range defined in inputs.
-        existing_points = ssn.query(Value) \
-            .filter(Value.time_series_id == self._id,
-                    Value.timestamp.in_(new_stamps)) \
-            .order_by(Value.timestamp).all()
-        # For range defined in inputs find old points or create new.
-        # Set new values.
-        for ind, stamp in enumerate(new_stamps):
-            point_to_set = None
-            for curr_point in existing_points:
-                if curr_point.timestamp > stamp:
-                    break
-                if curr_point.timestamp == stamp:
-                    point_to_set = curr_point
-                    break
-            if point_to_set is None:
-                point_to_set = Value(timestamp=stamp,
-                                     data_type=self._variable.data_type)
-                self._values.append(point_to_set)
-                point_to_set.set(values[ind])
+        return self._node_data.set_values(self._var_name,
+                                          self._ts_name,
+                                          start_label,
+                                          values)
 
     def get_values(self, start_label=None, length=None):
-        ssn = object_session(self)
-        if start_label is None:
-            # Get all points
-            points = ssn.query(Value) \
-                .filter(Value.time_series_id == self._id) \
-                .order_by(Value.timestamp).all()
-        else:
-            # Get timestamp from label.
-            start_point = ssn.query(TimePoint) \
-                .filter(TimePoint.time_scale_id == self._id,
-                        TimePoint.name == start_label).one_or_none()
-            if start_point is None:
-                raise Exception
-            else:
-                start = start_point.timestamp
-            # Get all points from start.
-            points = ssn.query(Value) \
-                .filter(Value.time_series_id == self._id,
-                        Value.timestamp >= start) \
-                .order_by(Value.timestamp).all()
-            # Limit length, if length is less than expected throw exception.
-            if length is not None:
-                if len(points) < length:
-                    raise Exception
-                points = points[:length]
-        # Return values of points found.
-        return [x.get() for x in points]
+        return self._node_data.get_values(self._var_name,
+                                          self._ts_name,
+                                          start_label,
+                                          length)
 
     def get_value(self, time_label):
-        return self.get_values(time_label, 1)
+        return self._node_data.get_values(self._var_name,
+                                          self._ts_name,
+                                          time_label,
+                                          1)
