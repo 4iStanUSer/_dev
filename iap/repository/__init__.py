92f9ab61c5fa82d8d1d1ab436bcc4cc87e05cf03
Dmitry Radkevich <dradkevich@4iconsult.com>
2016-08-19 13:28:34 +0300

    Added function for wh creation

--- iap/repository/__init__.py
+++ iap/repository/__init__.py
@@ -2,7 +2,10 @@
 Describe package here.
 """
 
+import pyramid
+
 from iap.repository.interface import iaccess, imanage_access
+from .db.warehouse import Warehouse
 
 from ..repository.storage import *
 from ..repository.interface.istorage import *
@@ -11,3 +14,9 @@ storage = Storage()
 backup = IBackup(storage)
 template = ITemplate(storage)
 istorage = IStorage(backup, template)
+
+
+def get_wh_interface():
+    reg = pyramid.threadlocal.get_current_registry()
+    wh = Warehouse(reg['dbsession_factory'])
+    return wh
