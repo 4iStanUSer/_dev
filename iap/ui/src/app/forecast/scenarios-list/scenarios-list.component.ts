51b5542010264ec6613ef34e8fdfc6112ad5127b
Viktor Yakusha <Viktor Yakusha>
2017-02-17 14:03:53 +0200

    VY_add_scenarios_page -- add initUserPermissions

--- iap/ui/src/app/forecast/scenarios-list/scenarios-list.component.ts
+++ iap/ui/src/app/forecast/scenarios-list/scenarios-list.component.ts
@@ -14,7 +14,8 @@ const DATEFORMAT = 'MM.DD.YY';
 })
 export class ScenariosListComponent implements OnInit {
     scenariosList: any[];
-    user_permissions: any;
+    user_permissions: any[]; // remove
+    userPermissionsData: any[];
     selectedScenarios: any[];
     favoriteList: any;
     selectElement: any;
@@ -22,17 +23,29 @@ export class ScenariosListComponent implements OnInit {
     search: string;
 
     //Permissions
-    createNewPermissionStatus:boolean = false;
-    finalizePermissionStatus:boolean = false;
-    sharePermissionStatus:boolean = false;
-    duplicatePermissionStatus:boolean = false;
-    deletePermissionStatus:boolean = false;
+    userPermissions: any = {
+        create: false,
+        finalize: false,
+        share: false,
+        copy: false,
+        delete: false
+    };
 
     constructor(private req: AjaxService) { }
 
     // Parse date 2017-02-10 14:00:13.990018 to DATEFORMAT
     dateParse(dateString: string) {
         return moment(Date.parse(dateString)).format(DATEFORMAT);
+    }
+
+    __getKey(key:string, obj:any, def:any=false) {
+        let value = def;
+        if (obj !== null && typeof obj === 'object') {
+            if (obj.hasOwnProperty(key)) {
+                value = obj[key];
+            }
+        }
+        return value;
     }
 
     in_array(what, where) {
@@ -42,29 +55,15 @@ export class ScenariosListComponent implements OnInit {
         return false;
     }
 
-    __clearTableRowSelect() {
-        let rows = document.getElementsByClassName('c-scenarios-table__row');
-        for (let i = 0; i < rows.length; i++) {
-            if (this.in_array('c-scenarios-table__row--active', rows[i].classList)) {
-                rows[i].classList.remove("c-scenarios-table__row--active");
-            }
-        }
+    ngOnInit(): void {
+        this.getScenariosList();
+        this.__loadUserPermissionsData();
+        this.selectedScenarios = [];
     }
 
-    __checkScenario(id: number) {
-        try {
-            this.selectedScenarios.push(id);
-        } catch(e) {
-            console.log(e);
-        }
-
-    }
-
-    __uncheckScenario(id: number) {
-        let index = this.selectedScenarios.indexOf(id);
-        if (index !== -1) {
-            this.selectedScenarios.splice(index, 1);
-        }
+    ngDoCheck(): void {
+        // Check permissions
+        this.__initUserPermissions();
     }
 
     // Get scenarios list from server
@@ -75,99 +74,8 @@ export class ScenariosListComponent implements OnInit {
         }).subscribe((data) => {
             this.scenariosList = data.data;
         });
+        console.log('---getScenariosList');
     }
-
-    // Get user permissions from server
-    getUserPermissions() {
-        this.req.post({
-            url_id: '/forecast/get_scenario_page',
-            data: {'filter': {}},
-        }).subscribe((data) => {
-            this.user_permissions = data.user_permission;
-        });
-    }
-
-    // Get scenario from server
-    getScenarioDetails(scenario_id: number) {
-        this.req.post({
-            url_id: '/forecast/get_scenario_details',
-            data: {'id': scenario_id},
-        }).subscribe((data) => {
-            this.selectScenario = data;
-        });
-    }
-
-    __getScenario(scenario_id: number) {
-        let scenario = {};
-        for (const i in this.scenariosList) {
-            if (this.scenariosList[i].id == scenario_id) {
-                scenario = this.scenariosList[i];
-                break;
-            }
-        }
-        return scenario;
-    }
-
-    __createScenario(new_scenario: any) {
-        this.req.post({
-            url_id: '/forecast/create_scenario',
-            data: new_scenario,
-        }).subscribe((data) => {
-            if(!data.error) {
-                this.scenariosList = [];
-                this.selectedScenarios = [];
-                this.getScenariosList();
-            }
-        });
-    }
-
-    __editScenario(edit_scenarios: any[]) {
-        this.req.post({
-            url_id: '/forecast/edit_scenario',
-            data: edit_scenarios,
-        }).subscribe((data) => {
-            if(!data.error) {
-                this.scenariosList = [];
-                this.selectedScenarios = [];
-                this.getScenariosList();
-            }
-        });
-    }
-
-    __deleteScenario(scenario_ids: number[]) {
-        this.req.post({
-            url_id: '/forecast/delete_scenario',
-            data: {'id': scenario_ids},
-        }).subscribe((data) => {
-            if(!data.error) {
-                this.scenariosList = [];
-                this.selectedScenarios = [];
-                this.getScenariosList();
-            }
-        });
-    }
-
-    ngOnInit(): void {
-        this.getScenariosList();
-        this.getUserPermissions();
-        this.selectedScenarios = [];
-    }
-
-    ngDoCheck(): void {
-        this.checkSharePermission();
-        this.getFavoriteList();
-        this.getSharedCount();
-        this.getLocalCount();
-        this.getFinalStatusCount();
-        this.getNewStatusCount();
-        this.getDraftsStatusCount();
-        this.getAuthorsList();
-
-        // Check permissions
-        this.checkCreateNewPermissionStatus();
-    }
-
-
     // ---------------------------------  Build filter  ----------------------------------//
     getFavoriteListLength() {
         try {
@@ -248,79 +156,86 @@ export class ScenariosListComponent implements OnInit {
     // ---------------------------------  Build filter  ----------------------------------//
 
     // -------------------------------  Check permissions  -------------------------------//
-    checkCreateNewPermissionStatus() {
-        this.createNewPermissionStatus = false;
-        if (this.user_permissions !== undefined && this.user_permissions.create) {
-            this.createNewPermissionStatus = true;
-        }
+    // Get user permissions from server
+    __loadUserPermissionsData() {
+        this.req.post({
+            url_id: '/forecast/get_scenario_page',
+            data: {'filter': {}},
+        }).subscribe((data) => {
+            this.userPermissionsData = data.user_permission;
+        });
     }
-    checkFinalizePermissionStatus() {
-        this.finalizePermissionStatus = false;
-        let curentFinalizePermissionStatus = 0;
-        if (this.selectedScenarios.length > 0) {
-            for (const i in this.selectedScenarios) {
-                if (this.in_array('change status', this.__getScenario(this.selectedScenarios[i]).scenario_permission) && this.__getScenario(this.selectedScenarios[i]).status.toLowerCase() !== 'final') {
-                    if (this.user_permissions !== undefined && this.user_permissions.finalize) {
-                        curentFinalizePermissionStatus ++;
-                    }
-                }
+
+    __initUserPermissions() {
+        this.__clearUserPermissions();
+        if (this.scenariosList !== undefined) {
+            // Check "Create New" permission
+            if (this.userPermissionsData !== undefined && this.__getKey('create', this.userPermissionsData)) {
+                this.userPermissions.create = true;
             }
-            if (curentFinalizePermissionStatus === this.selectedScenarios.length) {
-                this.finalizePermissionStatus = true;
-            }
-        }
-    }
-    checkSharePermission() {
-        this.sharePermissionStatus = false;
-        let curentSharePermissionStatus = 0;
-        if (this.selectedScenarios.length > 0) {
-            for (const i in this.selectedScenarios) {
-                if (this.in_array('share', this.__getScenario(this.selectedScenarios[i]).scenario_permission) && !this.__getScenario(this.selectedScenarios[i]).shared) {
-                    if (this.user_permissions !== undefined && this.user_permissions.share) {
-                        curentSharePermissionStatus ++;
-                    }
-                }
-            }
-            if (curentSharePermissionStatus === this.selectedScenarios.length) {
-                this.sharePermissionStatus = true;
+            if (this.selectedScenarios.length > 0) {
+                // Check "Create New" permission
+                this.userPermissions.finalize = this.__checkScenariosPermission('change_status');
+                // Check "Create New" permission
+                this.__checkScenariosPermission('share', true);
+                // Check "Create New" permission
+                this.__checkScenariosPermission('duplicate', false, true);
+                // Check "Delete" permission
+                this.__checkScenariosPermission('delete');
             }
         }
     }
-    checkDuplicatePermission() {
-        this.duplicatePermissionStatus = false;
-        let curentDuplicatePermissionStatus = 0;
-        if (this.selectedScenarios.length > 0) {
-            for (const i in this.selectedScenarios) {
-                if (this.in_array('copy', this.__getScenario(this.selectedScenarios[i]).scenario_permission)) {
-                    if (this.user_permissions !== undefined && this.user_permissions.duplicate) {
-                        curentDuplicatePermissionStatus ++;
-                    }
-                }
-            }
-            if (curentDuplicatePermissionStatus === this.selectedScenarios.length && this.selectedScenarios.length === 1) {
-                this.duplicatePermissionStatus = true;
-            }
-        }
+
+    __clearUserPermissions() {
+        this.userPermissions.create = false;
+        this.userPermissions.finalize = false;
+        this.userPermissions.share = false;
+        this.userPermissions.duplicate = false;
+        this.userPermissions.delete = false;
     }
-    checkDeletePermission() {
-        this.deletePermissionStatus = false;
-        let curentDeletePermissionStatus = 0;
-        if (this.selectedScenarios.length > 0) {
-            for (const i in this.selectedScenarios) {
-                if (this.in_array('delete', this.__getScenario(this.selectedScenarios[i]).scenario_permission)) {
-                    if (this.user_permissions !== undefined && this.user_permissions.delete) {
-                        curentDeletePermissionStatus ++;
+
+    __checkScenariosPermission(perm:string, check_item:boolean=false, only_one:boolean=false) {
+        let curentStatus = false;
+        let curentCheckCountStatus = 0;
+        const selectedScenariosList = this.__getSelectedScenariosList();
+        const permParamMap = {change_status: 'status', delete: 'delete', copy: 'copy', share: 'share'};
+        for (const i in selectedScenariosList) {
+            console.log(perm, this.userPermissions[perm], selectedScenariosList, this.__getKey('scenario_permission', this.__getScenario(selectedScenariosList[i]), {}));
+            if (this.in_array(perm, this.__getKey('scenario_permission', this.__getScenario(selectedScenariosList[i]), {}))) {
+                if (this.userPermissionsData[perm]) {
+                    if (check_item) {
+                        if (!this.__getKey(permParamMap[perm], this.__getScenario(selectedScenariosList[i]))) {
+                            curentCheckCountStatus ++;
+                        }
+                    } else {
+                        curentCheckCountStatus ++;
                     }
                 }
             }
-            if (curentDeletePermissionStatus === this.selectedScenarios.length) {
-                this.deletePermissionStatus = true;
+        }
+        if (curentCheckCountStatus === selectedScenariosList.length && selectedScenariosList.length > 0) {
+            if (only_one) {
+                if (selectedScenariosList.length === 1) {
+                    curentStatus = true;
+                }
+            } else {
+                curentStatus = true;
             }
         }
+        this.userPermissions[perm] = curentStatus;
     }
     // -------------------------------  Check permissions  -------------------------------//
 
     // --------------------------------  Scenario details  -------------------------------//
+    __clearTableRowSelect() {
+        let rows = document.getElementsByClassName('c-scenarios-table__row');
+        for (let i = 0; i < rows.length; i++) {
+            if (this.in_array('c-scenarios-table__row--active', rows[i].classList)) {
+                rows[i].classList.remove("c-scenarios-table__row--active");
+            }
+        }
+    }
+
     onSelectPreview(event: any) {
         if (this.in_array('c-scenarios-table__row', event.target.classList) || this.in_array('c-scenarios-table__row', event.target.parentNode.classList)) {
             if (this.in_array('c-scenarios-table__row', event.target.classList)) {
@@ -347,6 +262,16 @@ export class ScenariosListComponent implements OnInit {
 
     }
     // --------------------------------  Scenario details  -------------------------------//
+    // Get scenario from server
+    getScenarioDetails(scenario_id: number) {
+        this.req.post({
+            url_id: '/forecast/get_scenario_details',
+            data: {'id': scenario_id},
+        }).subscribe((data) => {
+            this.selectScenario = data;
+        });
+    }
+
     onToggleScenario(event: any) {
         let element = event.target;
         if (element.checked === true) {
@@ -354,6 +279,7 @@ export class ScenariosListComponent implements OnInit {
         } else {
             this.__uncheckScenario(parseInt(element.attributes['data-id'].value));
         }
+
         let allCheckScenario = document.getElementById('sc-table-all');
         if (this.selectedScenarios.length === this.scenariosList.length) {
             allCheckScenario.checked = true;
@@ -362,10 +288,7 @@ export class ScenariosListComponent implements OnInit {
         }
 
         // Check permissions
-        this.checkSharePermission();
-        this.checkFinalizePermissionStatus();
-        this.checkDuplicatePermission();
-        this.checkDeletePermission();
+        this.__initUserPermissions();
     }
 
     onToggleAllScenarios(event: any) {
@@ -385,32 +308,105 @@ export class ScenariosListComponent implements OnInit {
         }
 
         // Check permissions
-        this.checkSharePermission();
-        this.checkFinalizePermissionStatus();
-        this.checkDuplicatePermission();
-        this.checkDeletePermission();
+        this.__initUserPermissions();
     }
-
 
     onChangeAuthor(name: string) {
         console.log('---onChangeAuthor', name);
+    } /////////////////////////////////////////////////////////////////////
+    // ------------------------- ------  Scenario Actions  -------------------------------//
+    __getScenario(scenario_id: number) {
+        let scenario = {};
+        for (const i in this.scenariosList) {
+            if (this.scenariosList[i].id == scenario_id) {
+                scenario = this.scenariosList[i];
+                break;
+            }
+        }
+        return scenario;
     }
 
+    __createScenario(new_scenario: any) {
+        this.req.post({
+            url_id: '/forecast/create_scenario',
+            data: new_scenario,
+        }).subscribe((data) => {
+            if(!data.error) {
+                this.scenariosList = [];
+                this.selectedScenarios = [];
+                this.getScenariosList();
+            }
+        });
+    }
 
+    __editScenario(edit_scenarios: any[]) {
+        this.req.post({
+            url_id: '/forecast/edit_scenario',
+            data: edit_scenarios,
+        }).subscribe((data) => {
+            if(!data.error) {
+                this.scenariosList = [];
+                this.selectedScenarios = [];
+                this.getScenariosList();
+            }
+        });
+    }
 
+    __deleteScenario(scenario_ids: number[]) {
+        this.req.post({
+            url_id: '/forecast/delete_scenario',
+            data: {'id': scenario_ids},
+        }).subscribe((data) => {
+            if(!data.error) {
+                this.scenariosList = [];
+                this.selectedScenarios = [];
+                this.getScenariosList();
+            }
+        });
+    }
 
+    __getSelectedScenariosList() {
+        let selectedScenarios = [];
+        let allElements = document.querySelectorAll('*[id^="sc-table-check-"]');
+        for (let i = 0; i < allElements.length; i++) {
+            if (allElements[i].checked === true) {
+                selectedScenarios.push(parseInt(allElements[i].attributes['data-id'].value));
+            }
+        }
+        return selectedScenarios;
+    }
 
-    // -------------------------------------  Actions  -----------------------------------//
+    __checkScenario(id: number) {
+        try {
+            this.selectedScenarios.push(id);
+        } catch(e) {
+            console.log(e);
+        }
+
+    }
+
+    __uncheckScenario(id: number) {
+        let index = this.selectedScenarios.indexOf(id);
+        if (index !== -1) {
+            this.selectedScenarios.splice(index, 1);
+        }
+    }
+
     onToggleSharedScenario(event: any) {
         event.preventDefault();
         let edit_scenarios = [];
         const scenario_id = event.target.attributes['data-id'].value;
         let scenario = this.__getScenario(scenario_id);
+
+        scenario.shared = 'Yes';
+
+        /*
         if(this.in_array("share", scenario.scenario_permission)) {
             let new_shared = scenario.shared == 'No' ? 'Yes' : 'No';
             edit_scenarios.push({id: scenario.id, modify:[{value: new_shared, parameter: 'shared'}]});
             this.__editScenario(edit_scenarios);
         }
+        */
     }
 
     onToggleStatusScenario(event: any) {
@@ -454,10 +450,14 @@ export class ScenariosListComponent implements OnInit {
     }
 
     onDeleteScenario() {
-        if(this.deletePermissionStatus === true) {
+        if(this.userPermissions.delete === true) {
             this.__deleteScenario(this.selectedScenarios);
         }
+
+        // Check permissions
+        this.__initUserPermissions();
+
         return false;
     }
-    // -------------------------------------  Actions  -----------------------------------//
+    // ------------------------- ------  Scenario Actions  -------------------------------//
 }
